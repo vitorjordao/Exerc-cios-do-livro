b4ddf57d8017e2c09e142c0ec52d74630d092ea5
Vitor Jordão <srvitorlol@gmail.com>
2019-08-19 20:02:13 -0300

    Capitulo 7 terminado

--- Transacao.clj
+++ Transacao.clj
@@ -307,3 +307,144 @@
 (swap! registros conj
        {:valor 2700M :tipo "receita" :comentario "Bico"
         :moeda "R$" :data "01/12/2016"})
+
+(defn registrar [transacao] 
+  (swap! registros conj transacao))
+
+(registrar {:valor 33M :tipo "despesa" :comentario "Almoço"
+  :moeda "R$" :data "19/11/2016"})
+
+(registrar {:valor 45M :tipo "despesa" :comentario "Jogo no Steam"
+  :moeda "R$" :data "26/12/2016"})
+
+(def transacoes @registros)
+transacoes
+
+(defn saldo-acumulado [acumulado transacoes]
+  ;; if-let
+  (if-let [transacao (first transacoes)]
+    ;; se _transacao_ existir, continue calculando o saldo
+    (saldo-acumulado (if (despesa? transacao)
+                          (- acumulado (:valor transacao))
+                          (+ acumulado (:valor transacao)))
+                      (rest transacoes))
+    ;; se não existir, a coleção de transacoes acabou e é hora de
+    ;; retornar o resultado
+    acumulado))
+
+  (saldo-acumulado 0 transacoes) 
+
+;; e qual seria o resultado com uma lista sem transações?
+(saldo-acumulado 0 ())
+
+;; ou só os dois primeiros elementos?
+(saldo-acumulado 0 (take 2 transacoes))
+
+(defn calcular [acumulado transacao]
+  (let [valor (:valor transacao)]
+    (if (despesa? transacao)
+      (- acumulado valor)
+      (+ acumulado valor))))
+  
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (saldo-acumulado (calcular acumulado transacao)
+                      (rest transacoes))
+  acumulado))
+
+  (defn saldo-acumulado [acumulado transacoes]
+    (if-let [transacao (first transacoes)]
+      (do
+        (prn "Começou saldo-acumulado. Saldo até agora:" acumulado)
+        (saldo-acumulado (calcular acumulado transacao)
+                          (rest transacoes)))
+      (do
+        (prn "Processo encerrado. Saldo final:" acumulado)
+        acumulado)))
+
+(saldo-acumulado 0 transacoes)
+
+(defn saldo-acumulado [acumulado transacoes]
+  (prn "Começou saldo-acumulado. Saldo até agora:" acumulado)
+  (if-let [transacao (first transacoes)]
+    (do
+      (prn "Valor da transação atual:"
+        (valor-sinalizado transacao))
+      (prn "Quantidade de transações restantes:"
+        (count (rest transacoes)))
+      (prn)
+      (saldo-acumulado (calcular acumulado transacao)
+                        (rest transacoes)))
+    (do
+      (prn "Processo encerrado. Saldo final:" acumulado)
+      acumulado)))
+
+(saldo-acumulado 0 transacoes)
+
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (saldo-acumulado (calcular acumulado transacao)
+                      (rest transacoes))
+  acumulado))
+
+(defn saldo 
+  ([transacoes] 
+   (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if-let [transacao (first transacoes)]
+     (saldo (calcular acumulado transacao)
+                      (rest transacoes))
+     acumulado)))
+
+(saldo transacoes)
+
+(saldo 0 transacoes)
+
+(defn como-transacao [valor]
+  {:valor valor})
+
+(def poucas-transacoes
+  (map como-transacao (range 10)))
+
+(def muitas-transacoes
+  (map como-transacao (range 1000)))
+
+(def incontaveis-transacoes
+  (map como-transacao (range 100000)))
+
+(saldo poucas-transacoes)
+(saldo muitas-transacoes)
+(saldo incontaveis-transacoes)
+
+
+;; Com otimização em cauda
+(defn saldo
+  ([transacoes]
+   (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if (empty? transacoes)
+     acumulado
+     (saldo (calcular acumulado (first transacoes))
+                      (rest transacoes)))))
+
+(saldo poucas-transacoes)
+(saldo muitas-transacoes)
+(saldo incontaveis-transacoes)
+
+;; Com otimização em cauda
+(defn saldo
+  ([transacoes]
+   (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if (empty? transacoes)
+     acumulado
+;;    vvvvv aqui utilizamos recur em vez de saldo para aplicar a otimização
+     (recur (calcular acumulado (first transacoes))
+            (rest transacoes)))))
+
+(saldo poucas-transacoes)
+(saldo muitas-transacoes)
+(saldo incontaveis-transacoes)
+
+;;Tem como usar reduce
+(reduce calcular 0 transacoes)
